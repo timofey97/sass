6b3229cd978a3f804d6386f45caaf8cb326eb4ba
nanomen <nanomen@mail.ru>
2018-10-08 12:02:46 +0300

    fix path for building

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
-'use strict';
- 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
+  gulp.src('./sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
+// gulp.task('sass:watch', function () {
+//   gulp.watch('./sass/**/*.scss', ['sass']);
+// });
